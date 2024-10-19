10e7ea5c8b8b51cc5db0ce26f46f07757096d090
saakshijs <saakshijsgit@gmail.com>
2024-10-19 19:49:48 +0530

    IC

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express=require('express')
 
 const app=express()
 
-app.get('/',)
+app.get('/',(req,res)=>{
+    res.status(200).json({message:"Welcome to Blood Bank App",
+
+    });
+});
 
 
 const PORT=3000
